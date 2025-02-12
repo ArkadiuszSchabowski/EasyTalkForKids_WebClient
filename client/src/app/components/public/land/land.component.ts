c17a3ef849e58b015563ae455e8f240606a4fc22
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-02-12 13:28:05 +0100

    feat: update land component

--- client/src/app/components/public/land/land.component.ts
+++ client/src/app/components/public/land/land.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LandService } from 'src/app/_services/_land.service';
 
 @Component({
   selector: 'app-land',
   templateUrl: './land.component.html',
-  styleUrls: ['./land.component.scss']
+  styleUrls: ['./land.component.scss'],
 })
 export class LandComponent {
+  constructor(private landService: LandService, private router: Router) {}
 
+  setLand(landNumber: number) {
+    this.landService.landSubject.next(landNumber);
+    this.router.navigateByUrl(`/land/${landNumber}`);
+  }
 }
