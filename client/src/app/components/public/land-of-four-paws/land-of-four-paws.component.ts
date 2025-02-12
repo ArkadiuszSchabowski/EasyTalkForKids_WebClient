cd17b87a7f67d9db6b4f6c5339c3fb2c4a4df895
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-02-12 13:29:16 +0100

    feat: update land of four paws and land of frozen worlds components

--- client/src/app/components/public/land-of-four-paws/land-of-four-paws.component.ts
+++ client/src/app/components/public/land-of-four-paws/land-of-four-paws.component.ts
@@ -1,10 +1,106 @@
 import { Component } from '@angular/core';
+import { LandOfFourPawsService } from 'src/app/_services/land-of-four-paws.service';
+import { BaseLesson } from 'src/app/base/base-lesson';
 
 @Component({
   selector: 'app-land-of-four-paws',
   templateUrl: './land-of-four-paws.component.html',
-  styleUrls: ['./land-of-four-paws.component.scss']
+  styleUrls: ['./land-of-four-paws.component.scss'],
 })
-export class LandOfFourPawsComponent {
+export class LandOfFourPawsComponent implements BaseLesson {
+  title: string = 'Kraina czterech łapek';
 
+  constructor(private landOfFourPawsService: LandOfFourPawsService) {}
+
+  lesson1(): void {
+    this.landOfFourPawsService.lesson1();
+    console.log("lessson 1 - LandOfFourPaws")
+  }
+  lesson2(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson3(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson4(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson5(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson6(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson7(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson8(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson9(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson10(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson11(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson12(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson13(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson14(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson15(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson16(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson17(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson18(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson19(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson20(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson21(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson22(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson23(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson24(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson25(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson26(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson27(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson28(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson29(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson30(): void {
+    throw new Error('Method not implemented.');
+  }
 }
--- client/src/app/components/public/land-of-frozen-worlds/land-of-frozen-worlds.component.ts
+++ client/src/app/components/public/land-of-frozen-worlds/land-of-frozen-worlds.component.ts
@@ -1,10 +1,105 @@
 import { Component } from '@angular/core';
+import { BaseLesson } from 'src/app/base/base-lesson';
+import { LessonContent } from 'src/app/models/lesson-content';
 
 @Component({
   selector: 'app-land-of-frozen-worlds',
   templateUrl: './land-of-frozen-worlds.component.html',
-  styleUrls: ['./land-of-frozen-worlds.component.scss']
+  styleUrls: ['./land-of-frozen-worlds.component.scss'],
 })
-export class LandOfFrozenWorldsComponent {
+export class LandOfFrozenWorldsComponent implements BaseLesson {
+  title: string = 'Kraina zamarzniętych słów';
+
+  constructor() {}
+  lesson1(): void {
+    console.log('lessson 1 - LandOfFrozenWorlds');
+  }
+  lesson2(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson3(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson4(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson5(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson6(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson7(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson8(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson9(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson10(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson11(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson12(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson13(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson14(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson15(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson16(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson17(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson18(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson19(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson20(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson21(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson22(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson23(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson24(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson25(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson26(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson27(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson28(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson29(): void {
+    throw new Error('Method not implemented.');
+  }
+  lesson30(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
