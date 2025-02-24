7e6ef63597982018a58471021b911d186d97bc43
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-02-24 22:03:36 +0100

    feat: update land test imports

--- client/src/app/components/public/land/land.component.spec.ts
+++ client/src/app/components/public/land/land.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LandComponent } from './land.component';
 import { AppRoutingModule } from 'src/app/app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('LandComponent', () => {
   let component: LandComponent;
@@ -10,7 +11,7 @@ describe('LandComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [LandComponent],
-      imports: [AppRoutingModule]
+      imports: [AppRoutingModule, HttpClientModule]
     });
     fixture = TestBed.createComponent(LandComponent);
     component = fixture.componentInstance;
