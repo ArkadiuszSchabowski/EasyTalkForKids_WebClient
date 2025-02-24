d6a3d600cdb95f9b8bbb611e42b14962e2185bec
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-02-24 23:09:25 +0100

    chore: add http client module import

--- client/src/app/_services/land.service.spec.ts
+++ client/src/app/_services/land.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LandService } from './land.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('LandService', () => {
   let service: LandService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
     service = TestBed.inject(LandService);
   });
 
