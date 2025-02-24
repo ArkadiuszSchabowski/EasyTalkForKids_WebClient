85e51f3312cf3a616a40477781924495424e02dc
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-02-24 22:08:04 +0100

    feat: update module imports

--- client/src/app/modules/lesson/lesson.module.ts
+++ client/src/app/modules/lesson/lesson.module.ts
@@ -1,30 +1,30 @@
 import { NgModule } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '../material/material.module';
 
 import { DialogueComponent } from 'src/app/components/lesson/dialogue/dialogue.component';
-import { DialogueReviewComponent } from 'src/app/components/lesson/dialogue-review/dialogue-review.component';
+import { ReadingComponent } from 'src/app/components/lesson/reading/reading.component';
 import { SentenceComponent } from 'src/app/components/lesson/sentence/sentence.component';
-import { SentenceReviewComponent } from 'src/app/components/lesson/sentence-review/sentence-review.component';
 import { TheoryComponent } from 'src/app/components/lesson/theory/theory.component';
-import { VocabularyComponent } from 'src/app/components/lesson/vocabulary/vocabulary.component';
 import { VocabularyReviewComponent } from 'src/app/components/lesson/vocabulary-review/vocabulary-review.component';
+import { VocabularyComponent } from 'src/app/components/lesson/vocabulary/vocabulary.component';
 
 @NgModule({
   declarations: [
     DialogueComponent,
-    DialogueReviewComponent,
+    ReadingComponent,
     SentenceComponent,
-    SentenceReviewComponent,
     TheoryComponent,
     VocabularyComponent,
     VocabularyReviewComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, MaterialModule],
   exports: [
     DialogueComponent,
-    DialogueReviewComponent,
+    ReadingComponent,
     SentenceComponent,
-    SentenceReviewComponent,
     TheoryComponent,
     VocabularyComponent,
     VocabularyReviewComponent,
