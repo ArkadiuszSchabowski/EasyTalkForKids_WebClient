142c8a9d6f28cd85a3435e775c5efc57fe60d343
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-03-01 23:14:08 +0100

    chore: add AuthModule to imports

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/_navbar/navbar.component';
+import { AuthModule } from './modules/auth/auth.module';
 import { PublicModule } from './modules/public/public.module';
 import { MaterialModule } from './modules/material/material.module';
 import { LessonModule } from './modules/lesson/lesson.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
-
 import { HttpClientModule } from '@angular/common/http';
 
 
@@ -19,6 +19,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    AuthModule,
     LessonModule,
     MaterialModule,
     PublicModule,
