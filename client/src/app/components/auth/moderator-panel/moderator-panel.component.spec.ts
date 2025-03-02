16f1e250e1d3a9c61f936e4d7c258a88bba2a9d6
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-03-02 20:11:25 +0100

    feat: add http client module to tests

--- client/src/app/components/auth/moderator-panel/moderator-panel.component.spec.ts
+++ client/src/app/components/auth/moderator-panel/moderator-panel.component.spec.ts
@@ -4,6 +4,7 @@ import { ModeratorPanelComponent } from './moderator-panel.component';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from 'src/app/modules/material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ModeratorPanelComponent', () => {
   let component: ModeratorPanelComponent;
@@ -12,7 +13,7 @@ describe('ModeratorPanelComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ModeratorPanelComponent],
-      imports: [BrowserAnimationsModule, FormsModule, MaterialModule]
+      imports: [BrowserAnimationsModule, FormsModule, HttpClientModule, MaterialModule]
     });
     fixture = TestBed.createComponent(ModeratorPanelComponent);
     component = fixture.componentInstance;
