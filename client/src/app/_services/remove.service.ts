41a51560cabfa6b969231484cf39f6dd9751f1fb
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-03-09 17:58:44 +0100

    feat: add remove API call

--- client/src/app/_services/remove.service.ts
+++ client/src/app/_services/remove.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RemoveService {
+  apiUrl: string = environment.apiUrl;
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  remove(id: number, option: string) {
+    return this.http.delete(this.apiUrl + `${option}/${id}`);
+  }
 }
