b39a4f20a15d20346468c69565a4b727060da4a3
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-03-16 16:20:27 +0100

    feat: create topic form

--- client/src/app/components/auth/forms/add-topic/add-topic.component.ts
+++ client/src/app/components/auth/forms/add-topic/add-topic.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { AuthService } from 'src/app/_services/auth.service';
+import { AddTopicDto } from 'src/app/models/add/add-topic-dto';
+import { TopicError } from 'src/app/models/error/topic-error';
 
 @Component({
   selector: 'app-add-topic',
@@ -7,5 +10,27 @@ import { AuthService } from 'src/app/_services/auth.service';
   styleUrls: ['./add-topic.component.scss'],
 })
 export class AddTopicComponent {
+  model: AddTopicDto = new AddTopicDto();
+  errors: TopicError = new TopicError();
+
   constructor(public authService: AuthService) {}
+
+  createTopic(topicForm: NgForm) {
+    this.errors = new TopicError();
+
+    if (!this.model.polishName) {
+      this.errors.polishName = 'Polska nazwa tematu jest wymagana!';
+    }
+    if (!this.model.englishName) {
+      this.errors.englishName = 'Angielska nazwa tematu jest wymagana!';
+    }
+
+    if (this.errors.polishName || this.errors.englishName) {
+      return;
+    }
+
+    console.log(this.model);
+
+    topicForm.resetForm();
+  }
 }
