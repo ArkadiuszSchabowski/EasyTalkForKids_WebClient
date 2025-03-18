88416f73619b8207bb08f105182a7de6bb14db91
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-03-18 16:16:44 +0100

    chore: add ToastService Mock, imports and providers

--- client/src/app/components/auth/forms/add-category/add-category.component.spec.ts
+++ client/src/app/components/auth/forms/add-category/add-category.component.spec.ts
@@ -1,17 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddCategoryComponent } from './add-category.component';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/app/modules/material/material.module';
 
 describe('AddCategoryComponent', () => {
   let component: AddCategoryComponent;
   let fixture: ComponentFixture<AddCategoryComponent>;
+  let toastrService: ToastrService;
+
+  class MockToastrService {
+    success(message: string) {
+      console.log(message);
+    }
+  }
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AddCategoryComponent]
+      declarations: [AddCategoryComponent],
+      imports: [ToastrModule, FormsModule, MaterialModule, BrowserAnimationsModule],
+      providers: [
+        { provide: ToastrService, useClass: MockToastrService}
+      ]
     });
     fixture = TestBed.createComponent(AddCategoryComponent);
     component = fixture.componentInstance;
+    toastrService = TestBed.inject(ToastrService); 
     fixture.detectChanges();
   });
 
