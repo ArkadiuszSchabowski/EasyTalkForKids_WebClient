26713441dc784dce6b9885268d5d319fec9c49eb
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-03-19 11:57:30 +0100

    chore: add remove components to auth module

--- client/src/app/modules/auth/auth.module.ts
+++ client/src/app/modules/auth/auth.module.ts
@@ -12,6 +12,9 @@ import { ModeratorRemovePanelComponent } from 'src/app/components/auth/moderator
 import { AddLessonComponent } from 'src/app/components/auth/forms/add-lesson/add-lesson.component';
 import { AddWordComponent } from 'src/app/components/auth/forms/add-word/add-word.component';
 import { AddCategoryComponent } from 'src/app/components/auth/forms/add-category/add-category.component';
+import { RemoveCategoryComponent } from 'src/app/components/auth/forms/remove-category/remove-category.component';
+import { RemoveLessonComponent } from 'src/app/components/auth/forms/remove-lesson/remove-lesson.component';
+import { RemoveWordComponent } from 'src/app/components/auth/forms/remove-word/remove-word.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +24,9 @@ import { AddCategoryComponent } from 'src/app/components/auth/forms/add-category
     AddCategoryComponent,
     AddLessonComponent,
     AddWordComponent,
+    RemoveWordComponent,
+    RemoveCategoryComponent,
+    RemoveLessonComponent,
   ],
   imports: [CommonModule, FormsModule, MaterialModule, AuthRoutingModule],
   exports: [
@@ -30,6 +36,9 @@ import { AddCategoryComponent } from 'src/app/components/auth/forms/add-category
     AddCategoryComponent,
     AddLessonComponent,
     AddWordComponent,
+    RemoveWordComponent,
+    RemoveCategoryComponent,
+    RemoveLessonComponent,
   ],
 })
 export class AuthModule {}
